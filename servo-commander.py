13bb541b9beb3619a48ada9231649dbfe3840924
kuma35 <sindobook@gmail.com>
2016-02-26 17:07:07 +0900

    dryrun option and ack command.

--- servo-commander.py
+++ servo-commander.py
@@ -8,55 +8,89 @@ import time
 import serial
 import argparse
 
-def get_checksum(data) :
-    checksum = data[2]
-    for value in data[3:] :
-        checksum ^= value
-    return checksum
+class CmdServoException(object) :
+    """ CmdServo and sub class exception. """
 
-def append_checksum(data) :
-    checksum = data[2]
-    for value in data[3:] :
-        checksum ^= value
-    data.append(checksum)
-    return data
+    pass
 
-def recheck_checksum(data) :
-    checksum = data[2]
-    for value in data[3:-1] :
-        checksum ^= value
-        data[-1] = checksum
-    return data
+class CmdServo(object) :
+    """ Base class for servo commands. """
+    
+    def __init__(self) :
+        self.packet = []
+        self.recv = []
 
-def ack(ser, servo_id) :
-    snd = array.array('B', [0xFA, 0xAF, servo_id, 0x01, 0xFF, 0x00, 0x00])
-    snd = append_checksum(snd)
-    ser.write(snd)
-    rcv = ser.read()
-    return rcv
+    def get_checksum(self, data) :
+        """ calculate checksum byte from array. """
+        
+        checksum = data[2]
+        for value in data[3:] :
+            checksum ^= value
+        return checksum
 
-def info00(ser) :
-    snd = array.array('B', [0xFA, 0xAF, 0x01, 0x03, 0xFF, 0x00, 0x00])
-    snd = append_checksum(snd)
-    ser.write(snd)
-    rcv = []
-    for v in range(30) :
-        rcv[v] = ser.read()
-    return rcv
+    def prepare(self) :
+        pass
 
-def recv_packet(data) :
-    if (get_checksum(data[0:-1]) == data[-1]) :
-        print("checksum ok")
-        print("Header(FDDF):", pp.pprint(data[0:1]))
-        print("ID:", pp.pprint(data[2]))
-        print("Flags:", pp.pprint(data[3]))
-        print("Adress:", pp.pprint(data[4]))
-        print("Length:", pp.pprint(data[5]))
-        print("Count(1):", pp.pprint(data[6]))
-        print("Data:", pp.pprint(data[7:-2]))
-        print("Sum:", pp.pprint(data[-1]))
-    else :
-        print("checksum ng")
+    def execute(self) :
+        if self.packet is None:
+            raise CmdServoException("no prepare packet. coll prepare() before execute.");
+
+    def print(self, pp) :
+        """ pretty print recieve packet. """
+        pass
+
+class CmdAck(CmdServo) :
+    """ ACK command. """
+
+    def prepare(self, servo_id) :
+        self.packet = array.array('B',
+                                  [0xFA, 0xAF, servo_id, 0x01, 0xFF, 0x00, 0x00])
+        checksum = self.get_checksum(self.packet)
+        self.packet.append(checksum)
+        return self.packet
+
+    def execute(self, ser) :
+        CmdServo.execute()
+        ser.write(self.packet)
+        self.recv.append(ser.read())
+        return self.recv
+
+    def info(self, pp, data) :
+        print('ACK(\\x07):', end='')
+        pp.pprint(data)
+
+class CmdInfo00(CmdServo) :
+    """ get servo infomation memory addr.0- """
+
+    def prepare(self, servo_id) :
+        self.packet = array.array('B',
+                                  [0xFA, 0xAF, servo_id, 0x03, 0xFF, 0x00, 0x00])
+        cheksum = self.get_checksum(self.packet)
+        self.packet.append(checksum)
+        return self.packet
+
+    def execute(self, ser) :
+        ser.write(self.packet)
+        sleep(10)
+        for v in range(30) :
+            self.recv.append(ser.read())
+        return recv
+
+    def info(self, pp, data) :
+        if (self.get_checksum(data[2:-1]) == data[-1]) :
+            print("checksum ok")
+            print("Header(FDDF):", pp.pprint(data[0:1]))
+            print("ID:", pp.pprint(data[2]))
+            print("Flags:", pp.pprint(data[3]))
+            print("Adress:", pp.pprint(data[4]))
+            print("Length:", pp.pprint(data[5]))
+            print("Count(1):", pp.pprint(data[6]))
+            print("Data:", pp.pprint(data[7:-2]))
+            print("Checksum:", pp.pprint(data[-1]))
+        else :
+            print("invalid checksum.")
+            print("recive packet:", end='')
+            pp.pprint(data)
 
 def main() :
     parser = argparse.ArgumentParser(description='Manupirate FUTABA command servo RS405CB.')
@@ -71,61 +105,27 @@ def main() :
     parser.add_argument('-i', '--id',
                          dest='servo_id',
                          default=1)
+    parser.add_argument('--dryrun',
+                        action='store_true',
+                        help='no port open, no execute.')
+    parser.add_argument('command')
     args = parser.parse_args() # コマンドラインの引数を解釈します
-
-    
+    if args.command == 'ack' :
+        cmd = CmdAck()
+        cmd.prepare(args.servo_id)
+    else :
+        pass
     pp = pprint.PrettyPrinter(indent=4)
-    ser = serial.Serial(args.port, args.baud, timeout=1)
-    pp.pprint(ack(ser, args.servo_id))  # ACK return '\x07'
-    #info = info00(ser)
-    #recv_packet(info)
-    #
-    #トルクON
-    #snd = array.array('B', [0xfa, 0xaf, 0x01, 0x00, 0x24, 0x01, 0x01, 0x01, 0x24])
-    #ser.write(snd)
-    #rcv = ser.read()
-    #pp.pprint(rcv)
-    #time.sleep(1)
-    ##-90度
-    #snd = array.array('B', [0xfa, 0xaf, 0x01, 0x00, 0x1e, 0x02, 0x01, 0x7C, 0xFC])
-    #snd = append_checksum(snd)
-    #ser.write(snd)
-    #time.sleep(10)
-    ##トルクOFF
-    #snd = array.array('B', [0xfa, 0xaf, 0x01, 0x00, 0x24, 0x01, 0x01, 0x00])
-    #snd = append_checksum(snd)
-    #ser.write(snd)
-    #rcv = ser.read()
-    #pp.pprint(rcv)
-    #time.sleep(1)
-    ##
-    ser.close()
-#def main():
-#    u"""main routine"""
-#    console_encode = locale.getpreferredencoding()
-#    # parse command line
-#    opt_parser = OptionParser(usage=("%prog [options]"),
-#                              version="%prog 0.01")
-#    opt_parser.add_option("-p", "--port",
-#                          dest="port",
-#                          help=("specify port device."
-#                                "(default is /dev/ttyUSB0)"),
-#                          metavar="DEVICE")
-#    (options, arguments) = opt_parser.parse_args()
-#    ###########
-#    snd = array.array('B', [0xFA, 0xAF, 0x01, 0x01, 0xFF, 0x00, 0x00])
-#    cheksum = snd[2];
-#    for value in snd[3:] :
-#        checksum ^= value
-#    snd.append(checksum)
-#    #########
-#    ser = serial.Serial('/dev/ttyUSB0', 9600,
-#                        parity=serial.PARITY_NONE)
-#    ser.write(snd)
-#    rcv = ser.read(100)
-#    print rcv
-#
-#
-if __name__ == "__main__":
+    if args.dryrun :
+        print("====== DRY RUN. NOT EXECUTING =====")
+        print("generate packet:", end= '')
+        pp.pprint(cmd.packet)
+    else :
+        ser = serial.Serial(args.port, args.baud, timeout=1)
+        cmd.execute(ser)
+        cmd.info(pp)
+        ser.close()
+
+if __name__ == '__main__':
     main()
  
